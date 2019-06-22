d3298d5d837f4414137214aa2899162d41590617
Kirill <kirillkarpuk0@gmail.com>
2019-06-22 15:02:38 +0300

    added multi-module structure

---
+++ BLModule/src/main/java/com/itechart/contactsList/dto/AddressDTO.java
@@ -0,0 +1,69 @@
+package com.itechart.contactsList.dto;
+
+public class AddressDTO {
+
+    private long id;
+    private String country;
+    private String locality;
+    private String street;
+    private String house;
+    private short apartment;
+    private String postcode;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public short getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(short apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+}
